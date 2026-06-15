9b8f63f49df2e746929d23ab88691c7ff563a28f
RanjanDemo <ranjanmahato792@gmail.com>
2026-06-15 19:33:49 +0545

    task 5 is added

---
+++ Day1/Task5/bitwise.py
@@ -0,0 +1,9 @@
+a= 12
+b= 5
+print("Bitwise AND: ", a & b) 
+print("Bitwise OR: ", a | b)
+print("Bitwise XOR: ", a ^ b)
+print("Bitwise NOT: ", ~a)
+print("Left Shift: ", a << 2)
+print("Right Shift: ", a >> 2)
+   
